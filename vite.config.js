9f8ba3964858ea5ee1573ffc783c7e2352918690
Stiletti <stievus@web.de>
2024-02-06 11:35:52 +0100

    vite config nowtracks all htmlfiles

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve("./index.html"),
+        getgame: resolve("./getthegame.html"),
+        guides: resolve("./guides.html"),
+        social: resolve("./social.html"),
+        team: resolve("./team.html"),
+        impres: resolve("./impress.html"),
+      },
+    },
+  },
 });
